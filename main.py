8a33c20f47f5e6f44dc2347285c23dbeb4dd9c3d
Hemanth63052 <hemanthkumarpasham9502@gmail.com>
2025-07-31 16:30:02 +0530

    Step 4

--- main.py
+++ main.py
@@ -17,3 +17,21 @@ def add(numbers_str: str) -> int:
     return sum(actual_numbers)
 
 assert add("1\n5 , 6") == 12
+
+
+def add(numbers: str) -> int:
+    if not numbers:
+        return 0
+
+    delimiters = [",", "\n"]
+
+    if numbers.startswith("//"):
+        delimiter_line, numbers = numbers.split("\n", 1)
+        custom_delimiter = delimiter_line[2:]
+        delimiters = [custom_delimiter]
+
+    pattern = "|".join(map(re.escape, delimiters))
+    return sum(int(n) for n in re.split(pattern, numbers))
+
+assert add("//;\n1;2") == 3
+assert add("//#\n2#3#4") == 9
