9dc06df96a011ea39621ea9695395ba3efe91f63
Hemanth63052 <hemanthkumarpasham9502@gmail.com>
2025-07-31 16:35:47 +0530

    Step 4

--- main.py
+++ main.py
@@ -35,3 +35,34 @@ def add(numbers: str) -> int:
 
 assert add("//;\n1;2") == 3
 assert add("//#\n2#3#4") == 9
+
+def add(numbers: str) -> int:
+    if not numbers:
+        return 0
+
+    delimiters = [",", "\n"]
+
+    if numbers.startswith("//"):
+        delimiter_line, numbers = numbers.split("\n", 1)
+        custom_delimiter = delimiter_line[2:]
+        delimiters = [custom_delimiter]
+
+    pattern = "|".join(map(re.escape, delimiters))
+    parts = re.split(pattern, numbers)
+    values = []
+    for each_num in parts:
+        if each_num and int(each_num)>=0:
+            values.append(int(each_num))
+        else:
+            raise ValueError(f"negative numbers not allowed: {each_num}")
+
+    return sum(values)
+
+assert add("1,2,3") == 6
+assert add("//;\n4;5;6") == 15
+
+try:
+    add("1,-2,3,-4")
+except ValueError as e:
+    print(e)  # Output: negative numbers not allowed: -2,-4
+
