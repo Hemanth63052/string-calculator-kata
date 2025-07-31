5716ad7f6796d387506a99912b1642e2eacba9e9
Hemanth63052 <hemanthkumarpasham9502@gmail.com>
2025-07-31 16:20:37 +0530

    Step 3

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-
+import re
 
 def add(numbers_str: str) -> int:
     if not numbers_str:
         return 0
-    actual_numbers = [int(each_num) for each_num in numbers_str.split(",")]
+    actual_numbers = [int(each_num) for each_num in re.split(r"[,\n]", numbers_str)]
     return sum(actual_numbers)
 
-print(add("1,3,6,7"))
+assert add("1 \n 5 , 6") == 12
